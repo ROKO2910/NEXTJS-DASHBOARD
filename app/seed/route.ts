0f85b9fbcea1b5c9625849c8ba3ea0253096478a
ERLOGAS <lopez.2000erick@gmail.com>
2025-04-23 15:10:25 -0600

    first commit

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+{/*import {db} from '@vercel/postgres';*/}
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
